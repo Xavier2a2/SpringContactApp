7d6b0c44332d7ca54c4db9c1c0dda2d0c8ca61e4
Xavier2a2 <abanjoko@spartaglobal.com>
2021-03-17 11:05:56 +0000

    Starter App

---
+++ src/main/java/com/sparta/aaron/contactapp/ContactappApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.aaron.contactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactappApplication.class, args);
+    }
+
+}
